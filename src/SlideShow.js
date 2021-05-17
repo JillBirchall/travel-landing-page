623d0f180545a26debd7d28c32e1859d90dcac24
JillBirchall <jill.m.birchall@gmail.com>
2021-05-17 12:02:30 +0100

    Image slideshow functionality

--- src/SlideShow.js
+++ src/SlideShow.js
@@ -1,5 +1,78 @@
-import React from "react";
+import React, { useState } from "react";
+import Destinations from "./slideshow-data";
 
 export const SlideShow = () => {
-  return <div className="slideshow">Slideshow</div>;
+  const [currentDestination, setCurrentDestination] = useState(0);
+  const destinations = [...Destinations];
+
+  function changeSlide(newSlide) {
+    if (newSlide === currentDestination) return;
+    setCurrentDestination(newSlide);
+  }
+
+  function moveRight() {
+    let newSlide;
+    currentDestination === 2
+      ? (newSlide = 0)
+      : (newSlide = currentDestination + 1);
+    setCurrentDestination(newSlide);
+  }
+
+  function moveLeft() {
+    let newSlide;
+    currentDestination === 0
+      ? (newSlide = 2)
+      : (newSlide = currentDestination - 1);
+    console.log(newSlide);
+    setCurrentDestination(newSlide);
+  }
+
+  return (
+    <div className="slideshow-container">
+      <div
+        className="slideshow"
+        style={{
+          backgroundImage: `url(${destinations[currentDestination].image})`,
+        }}
+      >
+        <div
+          className="slideshow-chevron slideshow-chevron-left"
+          onClick={moveLeft}
+        ></div>
+        <div
+          className="slideshow-chevron slideshow-chevron-right"
+          onClick={moveRight}
+        ></div>
+        <h1 className="slideshow-text slideshow-country-text">
+          {destinations[currentDestination].country}
+        </h1>
+        <p className="slideshow-text slideshow-description-text">
+          {destinations[currentDestination].description}
+        </p>
+        <button className="slideshow-btn">
+          View {destinations[currentDestination].country} Tours
+        </button>
+        <div className="slideshow-circles">
+          <div
+            className={`${
+              currentDestination === 0 ? "filled-circle" : "empty-circle"
+            }`}
+            onClick={() => changeSlide(0)}
+          ></div>
+          <div
+            className={`${
+              currentDestination === 1 ? "filled-circle" : "empty-circle"
+            }`}
+            onClick={() => changeSlide(1)}
+          ></div>
+          <div
+            className={`${
+              currentDestination === 2 ? "filled-circle" : "empty-circle"
+            }`}
+            onClick={() => changeSlide(2)}
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
 };
---
+++ src/slideshow-data.js
@@ -0,0 +1,29 @@
+import Iceland from "./images/Iceland.jpg";
+import Indonesia from "./images/Indonesia.jpg";
+import CostaRica from "./images/costa-rica.jpg";
+
+const Destinations = [
+  {
+    id: 1,
+    country: "Iceland",
+    description:
+      "Chase the Northern Lights, go whale watching, scuba dive between tectonic plates, walk on a glacier, go inside a volcano!",
+    image: Iceland,
+  },
+  {
+    id: 2,
+    country: "Indonesia",
+    description:
+      "Scuba dive with manta rays or explore a shipwreck, climb volcanoes, visit Komodo National Park, tour temples and rice terraces.",
+    image: Indonesia,
+  },
+  {
+    id: 2,
+    country: "Costa Rica",
+    description:
+      "Go zip-lining, spot wildlife and colourful birds, snorkel and scuba dive, visit coffee plantation, relax in hot springs, watch baby sea turtles hatch",
+    image: CostaRica,
+  },
+];
+
+export default Destinations;
