5a1395cf463b16500fb68857e2e268f9e62206a3
JillBirchall <jill.m.birchall@gmail.com>
2021-07-13 15:48:03 +0100

    Fixing dropdown menus for larger touchscreens

--- src/NavLinkMenu.js
+++ src/NavLinkMenu.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-export const NavLinkMenu = ({ name, links }) => {
+export const NavLinkMenu = ({ name, links, isMenuOpen, toggleMenu }) => {
   return (
     <>
       <li className="navbar-btn">
-        <button className="dropdown-btn">{name}</button>
-        <div className="dropdown-content">
+        <button
+          className="dropdown-btn"
+          onClick={() => toggleMenu(name)}
+          onBlur={() => toggleMenu(name)}
+        >
+          {name}
+        </button>
+        <div
+          className={`${
+            isMenuOpen ? "dropdown-content display" : "dropdown-content"
+          }`}
+        >
           {links.map((link) => {
             return (
               <p className="dropdown-link" key={link.id}>
--- src/NavLinks.js
+++ src/NavLinks.js
@@ -1,21 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLinkMenu } from "./NavLinkMenu";
 import SubMenuLinks from "./data/submenu-data";
 
 export const NavLinks = () => {
+  const [currentMenu, setCurrentMenu] = useState("");
+
+  function toggleMenu(menuName) {
+    if (menuName === currentMenu) setCurrentMenu("");
+    //Close the submenu if it is already open
+    else setCurrentMenu(menuName);
+  }
+
   return (
     <div className="navbar-links">
       <ul className="left-navlinks">
         {SubMenuLinks.filter((menu, index) => index < 3).map((link) => {
           return (
-            <NavLinkMenu name={link.name} links={link.links} key={link.id} />
+            <NavLinkMenu
+              name={link.name}
+              links={link.links}
+              key={link.id}
+              isMenuOpen={currentMenu === link.name ? true : false}
+              toggleMenu={toggleMenu}
+            />
           );
         })}
       </ul>
       <ul className="right-navlinks">
         {SubMenuLinks.filter((menu, index) => index >= 3).map((link) => {
           return (
-            <NavLinkMenu name={link.name} links={link.links} key={link.id} />
+            <NavLinkMenu
+              name={link.name}
+              links={link.links}
+              key={link.id}
+              isMenuOpen={currentMenu === link.name ? true : false}
+              toggleMenu={toggleMenu}
+            />
           );
         })}
         <li className="navbar-link">
